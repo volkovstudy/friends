d0be097f37953e96a1a1c48a16a8b7662f7f6857
Volkov Daniil <volkov.study@mail.ru>
2022-09-08 20:09:46 +0300

    Add realization of FullName class

--- FullName/FullName.h
+++ FullName/FullName.h
@@ -1,9 +1,31 @@
 #ifndef FRIENDS_FULLNAME_H
 #define FRIENDS_FULLNAME_H
 
+#include "string"
+
+using namespace std;
 
 class FullName {
+private:
+    string Name;
+    string Age;
+    string Patronymic;
 
+public:
+    FullName(string InputName, string InputAge, string InputPatronymic)
+            : Name(InputName), Age(InputAge), Patronymic(InputPatronymic) {}
+
+    string GetName() {
+        return Name;
+    }
+
+    string GetAge() {
+        return Age;
+    }
+
+    string GetPatronymic() {
+        return Patronymic;
+    }
 };
 
 
