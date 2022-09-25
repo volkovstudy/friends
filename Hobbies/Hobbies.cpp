9f0db1f9a90155cc728ba1567aabab19573d0f30
Volkov Daniil <volkov.study@mail.ru>
2022-09-25 15:40:02 +0300

    Create methods & its realization in Hobbies class

--- Hobbies/Hobbies.cpp
+++ Hobbies/Hobbies.cpp
@@ -1,1 +1,40 @@
 #include "Hobbies.h"
+
+Hobbies::Hobbies(string hobby) : hobby(hobby) {}
+
+vector<string> Hobbies::getHobby(string hobby) {
+    hobbies.push_back(hobby);
+    return hobbies;
+}
+
+void Hobbies::setHobby() {
+    for (vector<string>::iterator it = hobbies.begin(); it != hobbies.end(); it++)
+        cout << *it << endl;
+}
+
+void Hobbies::findHobby(string pattern) {
+    for (vector<string>::iterator it = hobbies.begin(); it != hobbies.end(); it++) {
+        string hobby = *it;
+
+        if (hobby.find(pattern) != string::npos)
+            cout << hobby << endl;
+    }
+}
+
+vector<string> Hobbies::removeAllHobbies() {
+    hobbies.clear();
+    return hobbies;
+}
+
+vector<string> Hobbies::removeHobby(string hobby) {
+    string pattern = hobby;
+
+    for (vector<string>::iterator it = hobbies.begin(); it != hobbies.end(); it++) {
+        string friendHobby = *it;
+
+        if (friendHobby.find(pattern) != string::npos) {
+            hobbies.erase(it);  // Если найдено соответсвующее хобби, то удаляем его из вектора.
+            return hobbies;  // Возвращаем обновленный вектор.
+        }
+    }
+}
--- Hobbies/Hobbies.h
+++ Hobbies/Hobbies.h
@@ -1,10 +1,32 @@
 #ifndef FRIENDS_HOBBIES_H
 #define FRIENDS_HOBBIES_H
 
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<map>
+
+using namespace std;
 
 class Hobbies {
+private:
+    vector<string> hobbies; // Контейнер для хранения хобби друга.
+    string hobby;   // Любая вводимая информация, а также единичное представление хобби.
 
+public:
+    Hobbies(string hobby);
+
+    // Добавляет новое хобби в список хобби
+    vector<string> getHobby(string hobby);
+    // Выводит все хобби человека
+    void setHobby();
+    // Поиск всех хобби, имена которых содержат в себе определенный паттерн символов + их вывод.
+    void findHobby(string pattern);
+    // "Чистит" все хобби друга (скорее, вместе с самим товарищем) + возвращает пустой вектор.
+    vector<string> removeAllHobbies();
+    // Удаляет конкретное хобби из списка увлечений
+    vector<string> removeHobby(string hobby);
 };
 
 
-#endif //FRIENDS_HOBBIES_H
+#endif
