5ee534ff7974165e3a858a453722239090de41fd
Volkov Daniil <volkov.study@mail.ru>
2022-10-16 18:28:06 +0300

    Create findObjects() method

--- main.cpp
+++ main.cpp
@@ -14,11 +14,30 @@ vector<Friend*> findByPatronymic(string patronymic);
 void printVector(vector<Friend*> source);
 
 void initializeDatabase();
+void findObjects();
 
 int main() {
     initializeDatabase();
 
+    findObjects();
 
+    return 0;
+}
+
+void initializeDatabase() {
+    FullName* petyaFullName = new FullName("Petr", "Ivanov", "Ivanovich");
+    Friend* petya = new Friend(petyaFullName, "16.10.2000");
+
+    FullName* vasyaFullName = new FullName("Vasiliy", "Petrov", "Ivanovich");
+    Friend* vasya = new Friend(vasyaFullName, "01.01.1970");
+
+    database.push_back(petya);
+    database.push_back(vasya);
+
+    printVector(database);
+}
+
+void findObjects() {
     cout << "Finding by name \"Petr\"..." << endl;
     auto nameResults = findByName("Petr");
     cout << "Results:" << endl;
@@ -36,19 +55,4 @@ int main() {
     cout << "Results:" << endl;
     printVector(patronymicResults);
     cout << endl;
-
-    return 0;
-}
-
-void initializeDatabase() {
-    FullName* petyaFullName = new FullName("Petr", "Ivanov", "Ivanovich");
-    Friend* petya = new Friend(petyaFullName, "16.10.2000");
-
-    FullName* vasyaFullName = new FullName("Vasiliy", "Petrov", "Ivanovich");
-    Friend* vasya = new Friend(vasyaFullName, "01.01.1970");
-
-    database.push_back(petya);
-    database.push_back(vasya);
-
-    printVector(database);
 }
