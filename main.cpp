889a4a821d74ae5ba2f9c8263f3468f30e9ac50a
Volkov Daniil <volkov.study@mail.ru>
2022-10-16 17:55:11 +0300

    Create 2 instance of Friend in main()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
+#include "FullName/FullName.h"
+#include "Friend/Friend.h"
 
 using namespace std;
 
 int main() {
+    FullName* petyaFullName = new FullName("Petr", "Ivanov", "Ivanovich");
+    Friend* petya = new Friend(petyaFullName, "16.10.2000");
+
+    FullName* vasyaFullName = new FullName("Vasiliy", "Petrov", "Aleksandrov");
+    Friend* vasya = new Friend(vasyaFullName, "01.01.1970");
+
     return 0;
 }
